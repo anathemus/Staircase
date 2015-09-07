f67a195cecda05fb82a511f15b816262a9078b87
anathemus <anathemus@gmail.com>
2015-09-07 14:16:04 -0400

    Staircase
    Creates a staircase X number of lines.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staircase
+{
+    class Solution
+    {
+        static void Main(String[] args) 
+    {
+            int t = Convert.ToInt32(Console.ReadLine());
+            int a = t;
+
+            for (int i = 0; i < t; i++)
+            {
+                
+                string Staircase1 = "".PadLeft(a-1, ' ');
+                string Staircase2 = "".PadRight(i + 1, '#');
+                Console.Write(Staircase1);
+                Console.Write(Staircase2);
+                Console.WriteLine();
+                a--;
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
